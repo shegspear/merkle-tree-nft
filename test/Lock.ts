e54039f5196598fdc9ac26b43832d67a6a210330
shegspear <shegspear@gmail.com>
2024-09-18 18:49:02 +0100

    ran mutiple deployment test suite to checkconstructor state variable set

--- test/Lock.ts
+++ test/Lock.ts
@@ -49,6 +49,24 @@ describe("MerkleTree", function () {
       expect(await merkle.owner()).to.equal(owner);
     })
 
+    it("Should deploy with an nft address set", async function() {
+      const {merkle} = await loadFixture(deployMerkle);
+
+      expect(await merkle.nftTokenAddress()).to.equal("0xaBA7161A7fb69c88e16ED9f455CE62B791EE4D03");
+    })
+
+    it("Should deploy with a token address set", async function() {
+      const {merkle, token} = await loadFixture(deployMerkle);
+
+      expect(await merkle.tokenAddress()).to.equal(token);
+    })
+
+    it("Should deploy with merkle tree set", async function() {
+      const {merkle, owner} = await loadFixture(deployMerkle);
+
+      expect(await merkle.merkleRoot()).to.equal("0x13dbf15cb11aa819912f79df9027e4c225ad4543a5a19c4bff1e9e5b694b01e6");
+    })
+
   })
 
 });
